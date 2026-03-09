45103df585ea4b9979aa73e9d6b93d4a035ed50c
yusufulku <yusuf.ulku@eba.gov.tr>
2026-03-09 12:12:21 +0300

    fix: remove redirect loop and reset next.config

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.ankarasepetlivinckirala.com',
-          },
-        ],
-        destination: 'https://ankarasepetlivinckirala.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
