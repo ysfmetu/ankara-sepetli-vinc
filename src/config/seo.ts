41a7c8dde5b230c9b992212d4018b30d16fb268d
yusufulku <yusuf.ulku@eba.gov.tr>
2026-05-07 11:51:16 +0300

    feat:anasayfa title değişti

--- src/config/seo.ts
+++ src/config/seo.ts
@@ -1,8 +1,8 @@
 export const SEO_CONFIG = {
   baseUrl: 'https://ankarasepetlivinckirala.com',
   siteName: 'Ankara Sepetli Vinç Kiralama',
-  defaultTitle: 'Ankara Sepetli Vinç Kiralama | 7/24 Hızlı Hizmet',
-  defaultDescription: 'Ankara’da sepetli vinç kiralama hizmeti. 7/24 hızlı ulaşım, uygun fiyat, operatörlü vinç ve aynı gün yönlendirme. Hemen arayın: 0551 606 68 78',
+  defaultTitle: 'Ankara Sepetli Vinç Kiralama | 30 Dakikada Ulaşım ⚡ 0551 606 68 78',
+  defaultDescription: 'Ankara genelinde sepetli vinç kiralama hizmeti. 30 dakikada adresinize ulaşım, operatörlü vinç desteği ve uygun fiyat avantajı. Hemen arayın: 0551 606 68 78',
   twitterHandle: '@ankarasepetlivinc', // Varsa eklenebilir
   locale: 'tr_TR',
 };
